3297f05d08b82d4479de81ff7b9d52152e7d0aaf
HarrisonSayre <31598200+HarrisonSayre@users.noreply.github.com>
2024-01-24 02:29:24 -0500

    Update HexDecBinConverter.py

--- HexDecBinConverter.py
+++ HexDecBinConverter.py
@@ -1,6 +1,6 @@
 #Conversion between decimals, binary, and hexadecimal. Yes, python has some of these built in, this is my own implentation for practice.
 
-#Could use IsDecimal for 0-9, but not sure if it's faster. HexConvert looks more elegant this way, if nothing else
+#Dictionaries used for conversions involving hexadecimal.
 hexdigitdictionay = {"0":0, "1":1, "2":2,"3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9, "A":10, "B":11,"C":12, "D":13, "E":14, "F":15}
 hexdigitbinarydictonary = {"0": "0000", "1": "0001", "2": "0010", "3":"0011", "4":"0100", "5":"0101", "6":"0110", "7":"0111", "8":"1000", "9":"1001",
                            "A":"1010", "B":"1011", "C":"1100", "D":"1101", "E":"1110", "F":"1111"}
